82d60a92df67d1c88570fb0cf25a52166e48b411
Geonhwi Park <ghpark715@gmail.com>
2023-11-16 14:37:11 +0900

    #2 feat: WebClient로 날씨 정보 가져오기

--- src/main/java/cotato/findweather/controller/WeatherController.java
+++ src/main/java/cotato/findweather/controller/WeatherController.java
@@ -1,14 +1,41 @@
 package cotato.findweather.controller;
 
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class WeatherController {
 
+    @Service
+    public class WeatherService {
+
+        public String test() {
+            String stringMono = WebClient.create()
+                    .get()
+                    .uri(uriBuilder -> uriBuilder
+                            .scheme("https")
+                            .host("apihub.kma.go.kr")
+                            .path("/api/typ01/url/kma_sfcdd.php")
+                            .queryParam("tm", "20150715")
+                            .queryParam("stn", "0")
+                            .queryParam("help", "1")
+                            .queryParam("authKey", "PbPkF8c4Sraz5BfHOEq2Nw")
+                            .build())
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
+            return stringMono;
+
+        }
+
+    }
+
     @GetMapping("/weather")
     public String getWeather() {
-        // TODO : 날씨 정보 가져와 일교차 계산하기
-        return "최고 온도는 $ta_max 이고 최저 온도는 $tm_min 입니다. 일교차는 $diff 입니다.";
+        WeatherService weatherService = new WeatherService();
+        return weatherService.test();
     }
 }
