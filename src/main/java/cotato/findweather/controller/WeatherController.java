1be7501b70dceafca2f7b6ccff5b65cdbd1c7d53
Youth <boysoeng@naver.com>
2023-11-17 11:27:52 +0900

    feat: JSoup을 활용한 전날 일교차 구하는 기능

--- src/main/java/cotato/findweather/controller/WeatherController.java
+++ src/main/java/cotato/findweather/controller/WeatherController.java
@@ -1,14 +1,30 @@
 package cotato.findweather.controller;
 
+import cotato.findweather.dto.TemperatureDto;
+import cotato.findweather.service.WeatherService;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class WeatherController {
 
+    private final WeatherService weatherService;
+
     @GetMapping("/weather")
-    public String getWeather() {
+    public String getWeather() throws IOException {
         // TODO : 날씨 정보 가져와 일교차 계산하기
-        return "최고 온도는 $ta_max 이고 최저 온도는 $tm_min 입니다. 일교차는 $diff 입니다.";
+        TemperatureDto diff = weatherService.getDiff();
+        StringBuilder sb = new StringBuilder();
+        return sb.append("최고 온도는 ")
+                .append(diff.getMax())
+                .append(" 이고 최저 온도는 ")
+                .append(diff.getMin())
+                .append(" 입니다. 일교차는 ")
+                .append(diff.getMax() - diff.getMin())
+                .append("입니다.")
+                .toString();
     }
 }
